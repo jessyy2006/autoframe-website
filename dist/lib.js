2b909595752007029412f26aff51101e72358d99
jessyy2006 <worldlive145@gmail.com>
2025-07-03 13:50:09 +0200

    updated based on library updates

--- dist/lib.js
+++ dist/lib.js
@@ -4111,6 +4111,7 @@ var settings;
 var width;
 var height;
 var lastDetectionTime = 0;
+var sourceFrame;
 function autoframe(inputStream) {
   track = inputStream.getTracks()[0];
   settings = track.getSettings();
@@ -4123,10 +4124,11 @@ async function predictionLoop(inputStream) {
     lastDetectionTime = now;
     try {
       const detections = faceDetector.detectForVideo(
-        videoFrame(inputStream),
+        await videoFrame(inputStream),
         now
       ).detections;
-      processFrame(detections, inputStream);
+      sourceFrame = await videoFrame(inputStream);
+      processFrame(detections, inputStream, sourceFrame);
     } catch (err) {
       console.error("Error grabbing frame or detecting face:", err);
     }
@@ -4142,7 +4144,7 @@ var smoothedY = 0;
 var smoothedZoom = 0;
 var firstDetection = true;
 var oldFace = null;
-function processFrame(detections, inputStream) {
+function processFrame(detections, inputStream, sourceFrame2) {
   if (detections && detections.length > 0) {
     const newFace = detections[0].boundingBox;
     if (!oldFace) {
@@ -4166,7 +4168,7 @@ function processFrame(detections, inputStream) {
   topLeftY = Math.max(0, Math.min(topLeftY, height - cropHeight));
   ctx.drawImage(
     // doesnt take mediastream obj so trying with image bitmap instead
-    videoFrame(inputStream),
+    sourceFrame2,
     // source video
     // cropped from source
     topLeftX,
@@ -4270,7 +4272,6 @@ async function enableCam(event) {
     originalVideo.addEventListener("loadeddata", async (event2) => {
       framedVideo.srcObject = await autoframe(stream);
     });
-    const videoTrack = stream.getVideoTracks()[0];
   }).catch((err) => {
     console.error(err);
   });
