b582307afa2d13e67fd007c0b9fcca3cb5422dc4
mco <mathieu.cordebard@al-enterprise.com>
2025-07-10 10:54:47 +0200

    dev: houseworking

--- src/index.ts
+++ src/index.ts
@@ -2,33 +2,32 @@ import { AutoFramingConfig, AutoFramingLibrary } from "rainbow-auto-framing";
 
 // initialize library
 const library = new AutoFramingLibrary();
-await library.init("/config.json");
+await library.init("config.json");
 
 // DOM Setup
 let originalVideo: HTMLVideoElement = document.getElementById(
-  "originalVideo"
+	"originalVideo"
 ) as HTMLVideoElement;
 let framedVideo: HTMLVideoElement = document.getElementById(
-  "framedVideo"
+	"framedVideo"
 ) as HTMLVideoElement;
 let enableWebcamButton: HTMLButtonElement;
 
 // Check if webcam access is supported.
 const hasGetUserMedia = () => !!navigator.mediaDevices?.getUserMedia; // !! converts the result to true or false
-alert(hasGetUserMedia());
 
 if (hasGetUserMedia()) {
-  enableWebcamButton = document.getElementById(
-    "webcamButton"
-  ) as HTMLButtonElement;
-  enableWebcamButton.addEventListener("click", (event) => {
-    enableCam(event);
-    console.log("enabled cam");
-    // Remove the button.
-    enableWebcamButton.remove();
-  }); // When someone clicks this button, run the enableCam function
+	enableWebcamButton = document.getElementById(
+		"webcamButton"
+	) as HTMLButtonElement;
+	enableWebcamButton.addEventListener("click", (event) => {
+		enableCam(event);
+		console.log("enabled cam");
+		// Remove the button.
+		enableWebcamButton.remove();
+	}); // When someone clicks this button, run the enableCam function
 } else {
-  console.warn("getUserMedia() is not supported by your browser");
+	console.warn("getUserMedia() is not supported by your browser");
 }
 
 /**
@@ -36,39 +35,39 @@ if (hasGetUserMedia()) {
  * @param {event} event - event = click.
  */
 async function enableCam(event: Event) {
-  // getUsermedia parameters
-  const constraints = {
-    video: true,
-  };
+	// getUsermedia parameters
+	const constraints = {
+		video: true,
+	};
 
-  // Activate the webcam stream. need to flip video around 180 cuz mirrored rn
-  navigator.mediaDevices
-    .getUserMedia(constraints) // returns a Promise — meaning it's asynchronous
-    .then(function (stream) {
-      originalVideo.srcObject = stream;
-      console.log("og video assigned webcam stream");
-      originalVideo.play().catch((e) => {
-        console.warn("Video play failed:", e);
-      });
-      // When the video finishes loading and is ready to play, run the autoframe function.
-      originalVideo.addEventListener("loadeddata", async (event) => {
-        const { framedStream, width, height } = await library.autoframe(stream);
+	// Activate the webcam stream. need to flip video around 180 cuz mirrored rn
+	navigator.mediaDevices
+		.getUserMedia(constraints) // returns a Promise — meaning it's asynchronous
+		.then(function (stream) {
+			originalVideo.srcObject = stream;
+			console.log("og video assigned webcam stream");
+			originalVideo.play().catch((e) => {
+				console.warn("Video play failed:", e);
+			});
+			// When the video finishes loading and is ready to play, run the autoframe function.
+			originalVideo.addEventListener("loadeddata", async (event) => {
+				const { framedStream, width, height } = await library.autoframe(stream);
 
-        framedVideo.width = width;
-        framedVideo.height = height;
+				framedVideo.width = width;
+				framedVideo.height = height;
 
-        framedVideo.srcObject = framedStream;
+				framedVideo.srcObject = framedStream;
 
-        console.log(
-          `framedVideo w=${framedVideo.videoWidth}, h=${framedVideo.videoHeight}`
-        );
-        // ctx.drawImage isn't working, params are whack
-        framedVideo.play().catch((e) => {
-          console.warn("Video play failed:", e);
-        });
-      });
-    })
-    .catch((err) => {
-      console.error(err);
-    });
+				console.log(
+					`framedVideo w=${framedVideo.videoWidth}, h=${framedVideo.videoHeight}`
+				);
+				// ctx.drawImage isn't working, params are whack
+				framedVideo.play().catch((e) => {
+					console.warn("Video play failed:", e);
+				});
+			});
+		})
+		.catch((err) => {
+			console.error(err);
+		});
 }
