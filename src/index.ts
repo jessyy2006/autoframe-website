11e9ce7837026f962ba8ec2a62586ebe1a023823
mco <mathieu.cordebard@al-enterprise.com>
2025-07-10 15:22:07 +0200

    dev: adapt code to new library api

--- src/index.ts
+++ src/index.ts
@@ -1,32 +1,26 @@
-import { AutoFramingConfig, AutoFramingLibrary } from "rainbow-auto-framing";
+import { RainbowAutoFramingConfig, RainbowAutoFramingLibrary } from "rainbow-auto-framing";
 
 // initialize library
-const library = new AutoFramingLibrary();
-await library.init("config.json");
+const config = await loadConfig("config.json");
+const library = RainbowAutoFramingLibrary.create();
+library.init();
 
 // DOM Setup
-let originalVideo: HTMLVideoElement = document.getElementById(
-	"originalVideo"
-) as HTMLVideoElement;
-let framedVideo: HTMLVideoElement = document.getElementById(
-	"framedVideo"
-) as HTMLVideoElement;
-let enableWebcamButton: HTMLButtonElement;
+const originalVideo: HTMLVideoElement = document.getElementById("originalVideo") as HTMLVideoElement;
+const framedVideo: HTMLVideoElement = document.getElementById("framedVideo") as HTMLVideoElement;
+const enableWebcamButton: HTMLButtonElement = document.getElementById("webcamButton") as HTMLButtonElement;
 
 // Check if webcam access is supported.
 const hasGetUserMedia = () => !!navigator.mediaDevices?.getUserMedia; // !! converts the result to true or false
-
 if (hasGetUserMedia()) {
-	enableWebcamButton = document.getElementById(
-		"webcamButton"
-	) as HTMLButtonElement;
 	enableWebcamButton.addEventListener("click", (event) => {
 		enableCam(event);
 		console.log("enabled cam");
 		// Remove the button.
 		enableWebcamButton.remove();
 	}); // When someone clicks this button, run the enableCam function
-} else {
+} 
+else {
 	console.warn("getUserMedia() is not supported by your browser");
 }
 
@@ -36,9 +30,7 @@ if (hasGetUserMedia()) {
  */
 async function enableCam(event: Event) {
 	// getUsermedia parameters
-	const constraints = {
-		video: true,
-	};
+	const constraints = { video: true };
 
 	// Activate the webcam stream. need to flip video around 180 cuz mirrored rn
 	navigator.mediaDevices
@@ -51,23 +43,39 @@ async function enableCam(event: Event) {
 			});
 			// When the video finishes loading and is ready to play, run the autoframe function.
 			originalVideo.addEventListener("loadeddata", async (event) => {
-				const { framedStream, width, height } = await library.autoframe(stream);
+				const framedStream = library.autoframe(stream);
 
-				framedVideo.width = width;
-				framedVideo.height = height;
+				framedVideo.width = 640; //width;
+				framedVideo.height = 480; //height;
 
 				framedVideo.srcObject = framedStream;
 
-				console.log(
-					`framedVideo w=${framedVideo.videoWidth}, h=${framedVideo.videoHeight}`
-				);
+				console.log(`framedVideo w=${framedVideo.videoWidth}, h=${framedVideo.videoHeight}`);
 				// ctx.drawImage isn't working, params are whack
-				framedVideo.play().catch((e) => {
-					console.warn("Video play failed:", e);
-				});
+				
+				framedVideo.play().catch((e) => { console.warn("Video play failed:", e); });
 			});
 		})
 		.catch((err) => {
 			console.error(err);
 		});
+}
+
+async function loadConfig(config_path: string): Promise<RainbowAutoFramingConfig> {
+	// rejig so tht it takes autoframing config object as param, so that init() can access width/heigt
+	try {
+		// 1. Use fetch to get the JSON file
+		const response = await fetch(config_path);
+
+		// 2. Check if the network request was successful
+		if (!response.ok) throw new Error(`HTTP error! status: ${response.status} while fetching config.json`);
+
+		// 3. json() parses the JSON response into a JavaScript object
+		console.log("Config loaded successfully:", this.config);
+		return (await response.json()) as RainbowAutoFramingConfig;
+	}
+	catch (error) {
+		console.error("Error loading or parsing config.json:", error);
+		// might want to initialize with default settings if the config fails to load (should i?)
+	}
 }
